4b31234937735858352b12899e6d51105dc09bf9
Hari Prasanth <hariprasanth919@gmail.com>
2021-09-23 22:32:01 +0530

    about us

--- Damaram-Frontend/angular-damaram-frontend/src/app/app.component.ts
+++ Damaram-Frontend/angular-damaram-frontend/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-damaram-frontend';
+
+  toAbout() {
+    document.getElementById("box").scrollIntoView();
+  }
 }
