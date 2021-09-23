1f5c65cb67fdd6b910fb443a1a9a67ff6291bd03
Hari Prasanth <hariprasanth919@gmail.com>
2021-09-23 20:17:12 +0530

    75% completed home page

--- Damaram-Frontend/angular-damaram-frontend/src/app/app-routing.module.ts
+++ Damaram-Frontend/angular-damaram-frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { UserhomeComponent } from './userhome/userhome.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path:'login', component: LoginComponent}, 
   {path:'userhome', component: UserhomeComponent},
   {path:'home', component: HomeComponent}
